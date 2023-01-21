f42ef1bba5e0181f7fcaff754ae95b3407526af8
SantosMaycon <mayconalves.santos14@gmail.com>
2023-01-21 18:56:25 -0200

    chore: add player jump and animation run and jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
   [SerializeField] private float speed;
+  [SerializeField] private float jumpForce;
+  [SerializeField] private int amountOfJump;
   private Rigidbody2D rigidbody2d;
+  private Animator animator;
+  private int _amountOfJump;
   // Start is called before the first frame update
   void Start() {
     rigidbody2d = GetComponent<Rigidbody2D>();
+    animator = transform.GetChild(0).GetComponent<Animator>();
+    _amountOfJump = amountOfJump;
   }
 
   // Update is called once per frame
-  void Update() {}
+  void Update() {
+    jump();
+  }
  
   void FixedUpdate() {
     move();
@@ -23,6 +31,24 @@ public class Player : MonoBehaviour {
     if (horizontal != 0) {
       rigidbody2d.velocity = new Vector2(horizontal * speed, rigidbody2d.velocity.y);
       transform.localScale = new Vector2(Mathf.Sign(horizontal), transform.localScale.y);
+    }
+
+    animator.SetBool("isRun", horizontal != 0);
+  }
+
+  void jump() {
+    if (Input.GetButtonDown("Jump") && amountOfJump > 0) {
+      rigidbody2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+      animator.SetBool("isJump", true);
+      amountOfJump--;
+    }
+  }
+
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    if (other.gameObject.layer == 8) {
+      animator.SetBool("isJump", false);
+      amountOfJump = _amountOfJump;
     }
   }
 }
